3f84576b58c962ef80d641396ace663e5e0f2a24
Alex Dahl <adahl1@umbc.edu>
2017-04-17 17:12:22 -0400

    Fixed typo

--- src/world.cpp
+++ src/world.cpp
@@ -771,7 +771,7 @@ void World::computeGridForces() {
             Matrix2d gradient = p.gradientE*p.gradientP; 
             /// Matrix2d gradient = p.gradientE;
             double J = gradient.determinant();
-            if(j < 0) {
+            if(J < 0) {
                 printf("Negative Determinant: %d\n", J);
                 std::cout << "Gradient:\n" << gradient << "\n";
                 exit(1);
