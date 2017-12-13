1c35c94e8f2e71998f28802b6056f6127d35fb10
Alex Dahl <adahl1@umbc.edu>
2017-12-12 21:18:35 -0500

    Starting compile for OSX

--- compile.sh
+++ compile.sh
@@ -3,12 +3,19 @@
 if [ -z ${1+x} ]
 then
     BUILD_TYPE=-DCMAKE_BUILD_TYPE=Release
-elif [ ${1,,} == "debug" ]
+#elif [ ${1,,} == "debug" ]
+elif [ $(echo "${1}" | tr '[:upper:]' '[:lower:]') == "debug" ]
 then
     BUILD_TYPE=-DCMAKE_BUILD_TYPE=Debug
 fi
 mkdir -p build
 cd build
+unameOut="$(uname -s)"
+if [ $unameOut == "Darwin" ]
+then
+    export CC=/usr/local/bin/gcc-7
+    export CXX=/usr/local/bin/g++-7
+fi
 cmake -Wno-dev -DCMAKE_PREFIX_PATH=/usr/local/ "$BUILD_TYPE" ..
-make
+make VERBOSE=1
 cd ..
